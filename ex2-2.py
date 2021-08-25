4bfd5b525e3ec02fce7258340317790cc4d3d55f
htts1049 <htts1049@gmail.com>
2021-08-25 15:39:19 +0900

    ex2-2.py

--- ex2-2.py
+++ ex2-2.py
@@ -3,6 +3,6 @@ def remove_front_end(str):
     sen = sen.replace(str[len(sen)],'')
     return sen
 
-n = input("문자열을 입력하시오. : ")
+N = input("문자열을 입력하시오. : ")
 
-print("처음과 마지막 문자를 제거했습니다. : ", remove_front_end(n))
+print("처음과 마지막 문자를 제거했습니다. : ", remove_front_end(N))
