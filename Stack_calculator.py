2b14d4bb62bd82203ec37dc7155b2d2a82b1d15b
htts1049 <htts1049@gmail.com>
2021-09-02 00:43:27 +0900

    Stack_calculator.py

---
+++ Stack_calculator.py
@@ -0,0 +1,31 @@
+# 스택을 이용한 계산기
+
+the_stack = []
+
+def push(v):
+    the_stack.append(v)
+
+def pop():
+    return the_stack.pop()
+
+def main():
+    s = input('Enter RPN string: ')
+    a_list = s.split()
+
+    for item in a_list:
+        if item in '+-*/':
+            op2 = pop()
+            op1 = pop()
+            if item == '+':
+                push(op1 + op2)
+            elif item == '-':
+                push(op1 - op2)
+            elif item == '*':
+                push(op1 * op2)
+            else:
+                push(op1 / op2)
+        else:
+            push(float(item))
+    print(pop())
+
+main()
