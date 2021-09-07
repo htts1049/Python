9cd2a4ac580ddc7d9acd9faa50ee0a3892e5ca75
htts1049 <htts1049@gmail.com>
2021-09-07 23:31:45 +0900

    difference map and for

---
+++ map.py
@@ -0,0 +1,17 @@
+# map( function, iterable )
+
+my_list = [1, 2, 3, 4, 5]
+
+# for 반복문 이용
+result1 = []
+for val in my_list:
+    result1.append(val+1)
+    
+print(f'for 반복문 이용 : {result1}')
+
+
+
+# map 함수 이용
+result2 = list(map( lambda x: x+1 , my_list) )
+
+print(f'map 함수 이용 : {result2}')
